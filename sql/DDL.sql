e1f1e51c14f171102e1369267f4e0bfc58be1b25
test <test@gmail.com>
2024-09-09 08:06:04 +0800

    修改多個table

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE IF NOT EXISTS stock;
+CREATE DATABASE IF NOT EXISTS stock
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+;
 
 USE stock;
 
@@ -9,7 +12,7 @@ CREATE TABLE IF NOT EXISTS market (
 	PRIMARY KEY (id)
 )
 COMMENT='市場類別'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS industry (
@@ -19,15 +22,15 @@ CREATE TABLE IF NOT EXISTS industry (
 	PRIMARY KEY (`market_id`,`id`) USING BTREE
 )
 COMMENT='產業類別'
- ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ ENGINE=InnoDB4;
 
 CREATE TABLE IF NOT EXISTS company (
 	id INT NOT NULL AUTO_INCREMENT,
 	stock_code VARCHAR(20) NOT NULL COMMENT '股票代碼',
 	company_name VARCHAR(256) NOT NULL,
 	short_name VARCHAR(256) NOT NULL COMMENT '簡稱',
-	market_id VARCHAR(10) NOT NULL,
-	industry_id VARCHAR(10) NOT NULL,
+	market_id VARCHAR(10) NOT NULL COMMENT '市場類別的id',
+	industry_id VARCHAR(10) NOT NULL COMMENT '產業類別的id',
 	
 	founding_date DATE  NULL COMMENT '成立日期',
 	ipo_date DATE  NULL COMMENT '上市日期',
@@ -37,54 +40,112 @@ CREATE TABLE IF NOT EXISTS company (
 	PRIMARY KEY (id)
 )
 COMMENT='公司資料'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS minute_price_2330_2024 (
-	source_time DATETIME NOT NULL,
-	latest_trade_price DECIMAL(6,4) NOT NULL,
-	trade_volume INT NOT NULL,
-	accumulate_trade_volume INT NOT NULL,
+	source_time DATETIME NOT NULL COMMENT '資料來源的時間',
+	`open` DECIMAL(6,4) NOT NULL COMMENT '開盤價',
+	high DECIMAL(6,4) NOT NULL COMMENT '最高價',
+	low DECIMAL(6,4) NOT NULL COMMENT '最低價',
+	`close` DECIMAL(6,4) NOT NULL COMMENT '收盤價',
+	`volume` BIGINT(6,4) NOT NULL COMMENT '交易量',
+	`adj_close` DECIMAL(6,4) NOT NULL COMMENT '調整後收盤價',
 	
 	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 
 	PRIMARY KEY (source_time)
 )
 COMMENT='2330在2024年的每5秒收盤價'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS day_price_2330 (
-	source_time DATETIME NOT NULL,
-	`open` DECIMAL(6,4) NOT NULL,
-	high DECIMAL(6,4) NOT NULL,
-	low DECIMAL(6,4) NOT NULL,
-	
+	source_time DATETIME NOT NULL COMMENT '資料來源的時間',
+	`open` DECIMAL(6,4) NOT NULL COMMENT '開盤價',
+	high DECIMAL(6,4) NOT NULL COMMENT '最高價',
+	low DECIMAL(6,4) NOT NULL COMMENT '最低價',
+	`close` DECIMAL(6,4) NOT NULL COMMENT '收盤價',
+	`volume` BIGINT(6,4) NOT NULL COMMENT '交易量',
+	`adj_close` DECIMAL(6,4) NOT NULL COMMENT '調整後收盤價',
+
 	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 
 	PRIMARY KEY (source_time)
 )
 COMMENT='2024年的每天收盤價'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
+
+
+
 
 CREATE TABLE IF NOT EXISTS observe_group(
 	id INT NOT NULL AUTO_INCREMENT,
+--    user_id INT INT NOT NULL,
+	name VARCHAR(255) NOT NULL COMMENT '條件名稱',
 	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 
 	PRIMARY KEY (id)
 )
 COMMENT='觀察的條件群組'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS observe_condition(
 	id INT NOT NULL AUTO_INCREMENT,
-	condi_type int NOT NULL,/*條件類型*/
-	condi_name VARCHAR(255) NOT NULL,/*條件名稱*/
-	remark VARCHAR(255) NOT NULL,
-	observe_group_id INT NOT NULL,/*觀察的群組id*/
+	condi_type int NOT NULL COMMENT '條件類型',
+	condi_name VARCHAR(255) NOT NULL COMMENT '條件名稱',
+	remark VARCHAR(255) NOT NULL COMMENT '備註',
+	observe_group_id INT NOT NULL COMMENT '觀察的群組id',
 	
 	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 
 	PRIMARY KEY (id)
 )
 COMMENT='要觀察的條件'
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `user`(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	account VARCHAR(20) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	email VARCHAR(40) NOT NULL,
+	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+	PRIMARY KEY (id)
+)
+
+
+CREATE TABLE IF NOT EXISTS user_stock_group(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL COMMENT '條件名稱',
+	user_id INT NOT NULL ,
+
+	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+	PRIMARY KEY (id)
+)
+COMMENT='使用者的股票群組'
+ENGINE=InnoDB
+
+CREATE TABLE IF NOT EXISTS stock_group(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL COMMENT '條件名稱',
+
+	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+	PRIMARY KEY (id)
+)
+COMMENT='股票群組'
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS stock_group_mapping(
+	stock_group_id INT NOT NULL COMMENT '股票群組的id',
+	company_id INT NOT NULL COMMENT '公司id',
+
+	update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+	PRIMARY KEY (stock_group_id, stock_id)
+)
+COMMENT='股票群組與股票的對應'
+ENGINE=InnoDB;
